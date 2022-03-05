0938c65bb6df40b968ada442c3235a7245c93754
KKimura <19808712+k1mny@users.noreply.github.com>
2022-03-05 13:42:23 +0900

    fix next setting

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.GITHUB_ACTIONS ? "/wordle3d" : "",
+  basePath: process.env.GITHUB_ACTIONS ? "/wor3dle" : "",
   trailingSlash: true,
 };
 
